27ac880c4b36255b5e012817b47084a2a8bfe458
Salim <vu2lid@gmail.com>
2022-08-01 00:09:12 -0400

    Minor additions to UtilityTest.java

--- src/test/java/org/example/UtilityTest.java
+++ src/test/java/org/example/UtilityTest.java
@@ -1,10 +1,17 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.example.Utility.getProperty;
+import static org.example.Utility.getResponseJSON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class UtilityTest {
 
@@ -25,6 +32,10 @@ class UtilityTest {
     }
 
     @Test
-    void getResponseJSON() {
+    void getResponseJSON() throws IOException {
+        String requestUrl = "https://api-v3.mbta.com/routes?filter[type]=0,1";
+        JSONObject jsonObj = Utility.getResponseJSON(requestUrl);
+        JSONArray jsonArray = jsonObj.getJSONArray("data");
+        assertNotEquals(0, jsonArray.length(), "Failed to get routes");
     }
 }
